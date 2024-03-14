0fb0361d74823c69aa3635f2e4f8d0e51d423556
Mbringj <mbringjeremy@gmail.com>
2024-03-14 09:23:14 +0100

    add chart finance in app

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -2,10 +2,11 @@ import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
+import {inter} from '../fonts';
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+    <div className={`${inter.className} flex h-full flex-col px-3 py-4 md:px-2`}>
       <Link
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-green-600 p-4 md:h-40"
         href="/"
