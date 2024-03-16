2a02770a2c4bff7417e11d91b979ab6080900244
Mbringj <mbringjeremy@gmail.com>
2024-03-16 01:50:01 +0100

    add function login and logout feature

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,27 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -23,10 +44,17 @@ export async function createInvoice(formData: FormData) {
   });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
-  await sql`
-  INSERT INTO invoices (customer_id, amount, status, date)
-  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-`;
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch( error) {
+    return {
+      message: "Database error not fetching"
+    }
+  }
+  
  revalidatePath('/dashboard/invoices');
  redirect('/dashboard/invoices');
 };
@@ -43,12 +71,18 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
  
   const amountInCents = amount * 100;
- 
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  } catch(error) {
+    return {
+      message: "Database error not fetching",
+    }
+  }
+  
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
